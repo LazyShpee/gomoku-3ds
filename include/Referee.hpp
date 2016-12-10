3af0cbaba10a7776a36a62d6cebf46586b39b844
plean <jean.plancher@epitech.eu>
2016-12-10 17:18:06 +0100

    enum direction

--- include/Referee.hpp
+++ include/Referee.hpp
@@ -1,13 +1,26 @@
 #ifndef REFEREE_H_
 # define REFEREE_H_
 
+static int s_direction[8][2] = { {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0} };
+
+typedef enum direction {
+  NORTH_WEST,
+  NORTH,
+  NORTH_EAST,
+  EAST,
+  SOUTH_EAST,
+  SOUTH,
+  SOUTH_WEST,
+  WEST,
+} e_direction;
+
 class Referee {
     protected:
-        char **board;
+	char (& board)[19][19];
 
         char vision(char *v, char x, char y, char direction, bool lookBack = false);
     public:
-        Referee(char board[19][19]);
+	Referee(char (& board)[19][19]);
         virtual ~Referee();
         bool CanPlace(char player, char x, char y);
         bool UpdateBoard(char x, char y);
@@ -16,7 +29,7 @@ class Referee {
 #endif // !REFEREE_H_
 
 /*
-**  6 7 8
-**  5   1
-**  4 3 2
+**  1 2 3
+**  8   4
+**  7 6 5
 */
